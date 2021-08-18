8472e82b36aa8982e2b63526581ef881dc7cc998
AartiMachha <77327410+AartiMachha@users.noreply.github.com>
2021-08-18 11:45:57 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ var keyImg, key;
 var ringImg, ring;
 var moneyImg, money;
 var sackImg, sack;
-var treasure, treasureImg;
+var Ctreasure, CtreasureImg, OtreasureImg;
 var notes, notesImg;
 var potion, potionImg;
 var potion2, potion2Img;
@@ -23,7 +23,8 @@ function preload(){
   ringImg = loadImage("ring.png");
   moneyImg = loadImage("money.png");
   sackImg = loadImage("sack.png");
-  treasureImg = loadImage("treasure.png");
+  CtreasureImg = loadImage("closetreasure.png");
+  OtreasureImg = loadImage("opentreasure.png");
   notesImg = loadImage("notes.png");
   potionImg = loadImage("potion.png");
   potion2Img = loadImage("potion2.png");
@@ -45,13 +46,12 @@ function setup() {
   thief = createSprite(570,650);
   thief.addAnimation("thiefUR.png", thiefstandingUR);
   thief.scale = 0.7;
-
-  thief.setCollider("circle",0,0,10);
-  thief.debug = true;
+  thief.setCollider("circle",0,0,20);
 
   diamond = createSprite(473,208,25,25);
   diamond.addImage("diamond.png", diamondImg);
   diamond.scale = 0.1;
+  diamond.setCollider("rectangle", 0,0, 40,40);
 
   key = createSprite(400,80);
   key.addImage("key.png", keyImg);
@@ -60,34 +60,44 @@ function setup() {
   ring = createSprite(570,208);
   ring.addImage("ring.png", ringImg);
   ring.scale = 0.1;
+  ring.setCollider("rectangle", 0,0, 40,40);
 
   money = createSprite(670,212);
   money.addImage("money.png", moneyImg);
   money.scale = 0.2;
+  money.setCollider("rectangle", 0,0, 40,40);
 
-  sack = createSprite(470,160);
+  sack = createSprite(250,480);
   sack.addImage("sack.png", sackImg);
-  sack.scale = 0.1;
+  sack.scale = 0.3;
+  sack.setCollider("rectangle", 0,0, 70,70);
+  sack.debug = true;
 
-  treasure = createSprite(265,500);
-  treasure.addImage("treasure.png", treasureImg);
+  treasure = createSprite(470,150);
+  treasure.addImage("closetreasure.png", CtreasureImg);
   treasure.scale = 0.3;
 
   notes = createSprite(920,250);
   notes.addImage("notes.png", notesImg);
   notes.scale = 0.1;
+  notes.setCollider("rectangle", 0,0, 40,40);
+
+  
 
   potion = createSprite(670,355);
   potion.addImage("potion.png", potionImg);
   potion.scale = 0.2;
+  potion.setCollider("rectangle", 0,0, 40,40);
 
   potion2 = createSprite(470,350);
   potion2.addImage("potion2.png", potion2Img);
   potion2.scale = 0.2;
+  potion2.setCollider("rectangle", 0,0, 40,40);
 
   potion3 = createSprite(565,350);
   potion3.addImage("potion3.png", potion3Img);
   potion3.scale = 0.1;
+  potion3.setCollider("rectangle", 0,0, 40,40);
 
   music = createSprite(886,430);
   music.addImage("music.png", musicImg);
@@ -95,91 +105,63 @@ function setup() {
 
   door1 = createSprite(312,283,75,10);
   door1.shapeColor = "blue";
-
   door2 = createSprite(790,281,45,10);
   door2.shapeColor = "blue";
-
   door3 = createSprite(792,378,45,10);
   door3.shapeColor = "blue";
-
   door4 = createSprite(696,450,10,35);
   door4.shapeColor = "blue";
-
   door5 = createSprite(441,450,10,35);
   door5.shapeColor = "blue";
-
   door6 = createSprite(440,307,10,30);
   door6.shapeColor = "blue";
-
   door7 = createSprite(696,258,10,30);
   door7.shapeColor = "blue";
-
   door8 = createSprite(696,116,10,35);
   door8.shapeColor = "blue";
 
   wall1 = createSprite(372,47,390,10);
   wall1.shapeColor = 0;
-  
   wall2 = createSprite(760,47,260,10);
   wall2.shapeColor = 0;
-
   wall3 = createSprite(825,520,265,10);
   wall3.shapeColor = 0;
-
   wall4 = createSprite(313,520,270,10);
   wall4.shapeColor = 0;
-
   wall5 = createSprite(182,285,10,475);
   wall5.shapeColor = 0;
-
   wall6 = createSprite(952,308,10,434);
   wall6.shapeColor = 0;
-
   wall7 = createSprite(440,170,10,238);
   wall7.shapeColor = 0;
-  
   wall8 = createSprite(440,375,10,100);
   wall8.shapeColor = 0;
-
   wall9 = createSprite(440,375,10,100);
   wall9.shapeColor = 0;
-
   wall10 = createSprite(442,495,10,50);
   wall10.shapeColor = 0;
-
   wall11 = createSprite(696,495,10,50);
   wall11.shapeColor = 0;
-
   wall12 = createSprite(696,70,10,50);
   wall12.shapeColor = 0;
-
   wall13 = createSprite(696,187,10,100);
   wall13.shapeColor = 0;
-
   wall14 = createSprite(696,353,10,150);
   wall14.shapeColor = 0;
-
   wall15 = createSprite(572,189,255,10);
   wall15.shapeColor = 0;
-
   wall16 = createSprite(572,378,255,10);
   wall16.shapeColor = 0;
-
   wall17 = createSprite(725,378,85,10);
   wall17.shapeColor = 0;
-
   wall18 = createSprite(887,378,140,10);
   wall18.shapeColor = 0;
-
   wall19 = createSprite(887,281,140,10);
   wall19.shapeColor = 0;
-
   wall20 = createSprite(727,281,70,10);
   wall20.shapeColor = 0;
-
   wall21 = createSprite(227,281,90,10);
   wall21.shapeColor = 0;
-
   wall22 = createSprite(397,283,90,10);
   wall22.shapeColor = 0;
 
@@ -236,13 +218,64 @@ function draw() {
 
 
 
-  if(thief.collide(door1)){
-    door1.destroy();
+  if(thief.isTouching(diamond)){
+    diamond.remove();
+  }
+
+  if(thief.isTouching(ring)){
+    ring.remove();
+  }
+
+  if(thief.isTouching(notes)){
+    notes.remove();
+  }
+
+  /*if(thief.collide(key)){
+    key.destroy();
+    treasure.changeImage("opentreasure.png",OtreasureImg)
+  }*/
+
+  if(thief.isTouching(money)){
+    money.remove();
+  }
+
+  if(thief.isTouching(music)){
+    music.remove();
+  } 
+
+  if(thief.isTouching(sack)){
+    sack.remove();
+  }
+
+  if(thief.isTouching(potion)){
+    potion.remove();
+  }
+
+  if(thief.isTouching(potion3)){
+    potion3.remove();
+  }
+
+  if(thief.isTouching(potion2)){
+    potion2.remove();
   }
 
   
 
-  /*if (thief.isTouching(wall1)||thief.isTouching(wall2)||
+  /*if (thief.isTouching(door1)||thief.isTouching(door2)||
+  thief.isTouching(door3)||thief.isTouching(door4)||
+  thief.isTouching(door5)||thief.isTouching(door6)||
+  thief.isTouching(door7)||thief.isTouching(door8)){
+  thief.collide(door1) 
+  thief.collide(door2);
+  thief.collide(door3);
+  thief.collide(door4);
+  thief.collide(door5);
+  thief.collide(door6);
+  thief.collide(door7);
+  thief.collide(door8);
+  }*/
+
+  if (thief.isTouching(wall1)||thief.isTouching(wall2)||
   thief.isTouching(wall3)||thief.isTouching(wall4)||
   thief.isTouching(wall5)||thief.isTouching(wall6)||
   thief.isTouching(wall7)||thief.isTouching(wall8)||
@@ -275,8 +308,22 @@ function draw() {
   thief.collide(wall20);
   thief.collide(wall21);
   thief.collide(wall22);
-  }*/
+  }
    
- 
+  timer();
   drawSprites();
+}
+
+function timer(){
+  fill(0);
+  textSize(45);
+  text("Timer: "+ setInterval, 30,40);
+  
+  var timer = setInterval(function(){
+    document.getElementById('safeTimerDisplay').innerHTML='60:'+sec;
+    sec--;
+    if (sec < 0) {
+        clearInterval(timer);
+    }
+}, 1000);
 }
